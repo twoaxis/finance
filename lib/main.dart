908e77bb7a4e9ad2d41bfa8bc277a2d00dbf8620
Mohamed Eldahtoury <salintofficial@hotmail.com>
2024-10-14 03:52:23 +0300

    feat: add Firebase
    This commit sets up a Firebase project and connects the Flutter project
    to Firebase.
    
    This commit also changed the `minSdk` and `targetSdk` to versions
    supported by Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,15 @@ import 'package:financial_planner_mobile/pages/income.dart';
 import 'package:financial_planner_mobile/pages/liabilities.dart';
 import 'package:financial_planner_mobile/util/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
