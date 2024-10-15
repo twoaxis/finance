b626cb8e68dae467bf1733caaf0b872742200b4c
Ahmed Helmy <125504430+AhmedHelmy18@users.noreply.github.com>
2024-10-15 17:21:54 +0300

    add log out in menu (#5)

--- lib/ui/app/app.dart
+++ lib/ui/app/app.dart
@@ -3,6 +3,7 @@ import 'package:financial_planner_mobile/ui/app/screens/expenses.dart';
 import 'package:financial_planner_mobile/ui/app/screens/income.dart';
 import 'package:financial_planner_mobile/ui/app/screens/liabilities.dart';
 import 'package:financial_planner_mobile/util/theme.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class App extends StatefulWidget {
@@ -13,7 +14,6 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-
   int selected = 0;
 
   @override
@@ -31,15 +31,36 @@ class _AppState extends State<App> {
       appBar: AppBar(
         title: const Text("Financial Planner"),
         backgroundColor: lightTheme.surfaceContainer,
+        actions: [
+          PopupMenuButton(
+            onSelected: (value) async {
+              if (value == 'Log out') {
+                await FirebaseAuth.instance.signOut();
+              }
+            },
+            offset: Offset(0, 50),
+            itemBuilder: (BuildContext context) {
+              return [
+                PopupMenuItem(
+                  value: 'Log out',
+                  child: Text('Log out'),
+                ),
+              ];
+            },
+          ),
+        ],
       ),
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
         currentIndex: selected,
         items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.attach_money), label: "Income"),
-          BottomNavigationBarItem(icon: Icon(Icons.shopping_bag), label: "Expenses"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.attach_money), label: "Income"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_bag), label: "Expenses"),
           BottomNavigationBarItem(icon: Icon(Icons.house), label: "Assets"),
-          BottomNavigationBarItem(icon: Icon(Icons.payment), label: "Liabilities")
+          BottomNavigationBarItem(
+              icon: Icon(Icons.payment), label: "Liabilities")
         ],
         onTap: (index) {
           setState(() {
