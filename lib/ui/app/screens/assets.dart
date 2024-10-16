c17598300d4e030315ef13d379b8f0856ee301f5
Mohamed Eldahtoury <salintofficial@hotmail.com>
2024-10-16 06:57:36 +0300

    feat: add ability to delete assets
    This commit adds the ability to delete assets using the delete icon and
    adds a confirmation box.
    
    This commit also adds error handling for fetching the assets.

--- lib/ui/app/screens/assets.dart
+++ lib/ui/app/screens/assets.dart
@@ -14,6 +14,7 @@ class AssetsPage extends StatefulWidget {
 class _AssetsPageState extends State<AssetsPage> {
 
   List<dynamic> assets = [];
+  bool error = false;
 
   @override
   void initState() {
@@ -32,78 +33,117 @@ class _AssetsPageState extends State<AssetsPage> {
         assets = data?["assets"];
       });
     }
-    on FirebaseException catch (e) {
-
+    on Exception catch (e) {
+      setState(() {
+        error = true;
+      });
     }
   }
 
-
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      padding: const EdgeInsets.all(10),
-      itemCount: assets.length,
-      itemBuilder: (context, index) {
-        return Padding(
-          padding: const EdgeInsets.symmetric(vertical: 5),
-          child: Container(
-            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-            decoration: BoxDecoration(
-              color: lightTheme.surfaceContainer,
-              border: Border(
-                  bottom: BorderSide(
-                      color: lightTheme.surfaceDim,
-                      width: 3
-                  )
-              ),
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: Row(
-              children: [
-                Expanded(
-                  flex: 1,
-                  child: Text(
-                      assets[index]["name"],
-                      style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18
-                      )
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        error ? const Text("Error has occurred while fetching data.") : const SizedBox(height: 0),
+        Expanded(
+          child: ListView.builder(
+            padding: const EdgeInsets.all(10),
+            itemCount: assets.length,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(vertical: 5),
+                child: Container(
+                  padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                  decoration: BoxDecoration(
+                    color: lightTheme.surfaceContainer,
+                    border: Border(
+                        bottom: BorderSide(
+                            color: lightTheme.surfaceDim,
+                            width: 3
+                        )
+                    ),
+                    borderRadius: BorderRadius.circular(10),
                   ),
-                ),
-                Expanded(
-                    flex: 1,
-                    child: Center(
-                      child: Text(
-                          "\$${NumberFormat('#,##0').format(assets[index]["value"]).toString()}",
-                          style: TextStyle(
-                              color: lightTheme.surfaceTint,
-                              fontSize: 15
+                  child: Row(
+                    children: [
+                      Expanded(
+                        flex: 1,
+                        child: Text(
+                            assets[index]["name"],
+                            style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18
+                            )
+                        ),
+                      ),
+                      Expanded(
+                          flex: 1,
+                          child: Center(
+                            child: Text(
+                                "\$${NumberFormat('#,##0').format(assets[index]["value"]).toString()}",
+                                style: TextStyle(
+                                    color: lightTheme.surfaceTint,
+                                    fontSize: 15
+                                )
+                            ),
                           )
                       ),
-                    )
-                ),
-                Expanded(
-                  flex: 1,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      IconButton(
-                          onPressed: () {},
-                          icon: const Icon(Icons.delete)
-                      ),
+                      Expanded(
+                        flex: 1,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            IconButton(
+                                onPressed: () {
+                                  showDialog(context: context, builder: (BuildContext build) {
+                                      return AlertDialog(
+                                        title: const Text("Are you sure to delete this asset?"),
+                                        icon: const Icon(Icons.delete),
+                                        actions: [
+                                          TextButton(
+                                            onPressed: () {
+                                              Navigator.of(context).pop();
+                                            },
+                                            child: const Text("Cancel"),
+                                          ),
+                                          TextButton(
+                                            onPressed: () {
+                                              Navigator.of(context).pop();
 
-                      IconButton(
-                          onPressed: () {},
-                          icon: const Icon(Icons.edit)
+                                              setState(() {
+                                                assets.removeAt(index);
+                                              });
+
+                                              // TODO: Error handling.
+                                              FirebaseFirestore.instance.collection("users").doc(FirebaseAuth.instance.currentUser?.uid).update({
+                                                "assets": assets
+                                              });
+                                            },
+                                            child: const Text("Yes"),
+                                          ),
+                                        ],
+                                      );
+                                  });
+                                },
+                                icon: const Icon(Icons.delete)
+                            ),
+          
+                            IconButton(
+                                onPressed: () {},
+                                icon: const Icon(Icons.edit)
+                            )
+                          ],
+                        ),
                       )
                     ],
                   ),
-                )
-              ],
-            ),
+                ),
+              );
+            },
           ),
-        );
-      },
+        ),
+      ],
     );
   }
 }
