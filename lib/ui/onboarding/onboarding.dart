ed89812063d5cf68fad85aa9f362d86030b559cb
Mohamed Eldahtoury <salintofficial@hotmail.com>
2025-02-16 12:10:59 +0200

    feat: update onboarding screen
    Co-authored-by: Mohamed Naser Mohamed Aldeeb <mhmaldyb510@gmail.com>

--- lib/ui/onboarding/onboarding.dart
+++ lib/ui/onboarding/onboarding.dart
@@ -1,5 +1,6 @@
 import 'package:financial_planner_mobile/ui/onboarding/screens/login.dart';
 import 'package:financial_planner_mobile/ui/onboarding/screens/signup.dart';
+import 'package:financial_planner_mobile/util/theme.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -11,7 +12,6 @@ class Onboarding extends StatefulWidget {
 }
 
 class _OnboardingState extends State<Onboarding> {
-
   int page = 0;
 
   void changePage(int x) {
@@ -23,15 +23,88 @@ class _OnboardingState extends State<Onboarding> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: IndexedStack(
-          index: page,
-          children: [
-            LoginPage(changePage: changePage),
-            SignupPage(changePage: changePage)
-          ],
-        )
-      )
-    );
+        body: Container(
+            width: double.infinity,
+            height: double.infinity,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+              image: AssetImage("asset/images/onboarding.jpg"),
+              fit: BoxFit.cover,
+                  alignment: Alignment(0.4, 0)
+            )),
+            child: Container(
+              padding: EdgeInsets.all(24),
+              width: double.infinity,
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      colors: [Colors.transparent, Color(0xFF000000)],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter)),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    "TwoAxis Finance",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold, fontSize: 35),
+                  ),
+                  Text(
+                    "Manage your income, expenses and assets with ease!",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  SizedBox(height: 20),
+                  SizedBox(
+                    width: double.infinity,
+                    height: 50,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: darkTheme.primary,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                          shadowColor: Colors.black,
+                          elevation: 3
+                      ),
+                      onPressed: () {
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => LoginPage()));
+                      },
+                      child: Text(
+                        "Login to your account",
+                        style: TextStyle(
+                          color: darkTheme.onPrimary
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10,),
+                  SizedBox(
+                    width: double.infinity,
+                    height: 50,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: darkTheme.surfaceContainer,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10)
+
+                          ),
+                        shadowColor: Colors.black,
+                        elevation: 3
+                      ),
+                      onPressed: () {
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => SignupPage()));
+                      },
+                      child: Text(
+                        "Create an account",
+                        style: TextStyle(
+                            color: darkTheme.onPrimary
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20,),
+                ],
+              ),
+            )));
   }
 }
