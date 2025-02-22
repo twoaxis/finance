2cb0cd3a25e75524b48b57eb4318825ff91da798
Mohamed Eldahtoury <salintofficial@hotmail.com>
2025-02-22 22:17:50 +0200

    build: add flutter ci (#28)
    * build: add flutter ci
    
    * refactor: address flutter analyze issues
    
    * build: add signing key workflow

--- lib/ui/app/assets/asset_action_button.dart
+++ lib/ui/app/assets/asset_action_button.dart
@@ -14,13 +14,9 @@ class _AssetActionButtonState extends State<AssetActionButton> {
   String error = "";
   TextEditingController nameController = TextEditingController();
   TextEditingController valueController = TextEditingController();
-  late BuildContext currentContext;
 
   @override
   Widget build(BuildContext context) {
-    setState(() {
-      currentContext = context;
-    });
     return IconButton(
         onPressed: () {
           showDialog(
@@ -84,8 +80,6 @@ class _AssetActionButtonState extends State<AssetActionButton> {
                                     error = "Please fill all fields";
                                   });
                                 } else {
-                                  final currentContext = context;
-
                                   await FirebaseFirestore.instance
                                       .collection("users")
                                       .doc(FirebaseAuth
@@ -102,9 +96,9 @@ class _AssetActionButtonState extends State<AssetActionButton> {
                                   nameController.clear();
                                   valueController.clear();
 
-                                  Navigator.of(currentContext).pop();
+                                  if(context.mounted) Navigator.of(context).pop();
                                 }
-                              } on Exception catch (e) {
+                              } on Exception {
                                 setState(() {
                                   error = "An error has occurred";
                                 });
--- lib/ui/app/settings/settings.dart
+++ lib/ui/app/settings/settings.dart
@@ -11,7 +11,6 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPageState extends State<SettingsPage> {
-
   var passwordController = TextEditingController();
   var pending = false;
   var error = "";
@@ -20,7 +19,8 @@ class _SettingsPageState extends State<SettingsPage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text("Settings",
+          title: const Text(
+            "Settings",
             style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
           ),
           backgroundColor: darkTheme.surfaceContainer,
@@ -32,78 +32,106 @@ class _SettingsPageState extends State<SettingsPage> {
                   style: TextStyle(color: darkTheme.error)),
               leading: Icon(Icons.delete, color: darkTheme.error),
               onTap: () {
-                showDialog(context: context, builder: (BuildContext widget) {
-                  return StatefulBuilder(builder: (context, setDialogState) {
-                    return AlertDialog(
-                      title: Text("Delete account?"),
-                      content: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Text("Enter your password to confirm account deletion?"),
-                          error.isNotEmpty
-                              ? Text(
-                            error,
-                            style: const TextStyle(color: Colors.red, fontSize: 20),
-                          )
-                              : const SizedBox(
-                            height: 20,
+                showDialog(
+                    context: context,
+                    builder: (BuildContext widget) {
+                      return StatefulBuilder(
+                          builder: (context, setDialogState) {
+                        return AlertDialog(
+                          title: Text("Delete account?"),
+                          content: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Text(
+                                  "Enter your password to confirm account deletion?"),
+                              error.isNotEmpty
+                                  ? Text(
+                                      error,
+                                      style: const TextStyle(
+                                          color: Colors.red, fontSize: 20),
+                                    )
+                                  : const SizedBox(
+                                      height: 20,
+                                    ),
+                              TextField(
+                                enabled: !pending,
+                                obscureText: true,
+                                controller: passwordController,
+                                textInputAction: TextInputAction.next,
+                                decoration: const InputDecoration(
+                                  hintText: "••••••••••",
+                                  labelText: "Password",
+                                ),
+                              )
+                            ],
                           ),
-                          TextField(
-                            enabled: !pending,
-                            obscureText: true,
-                            controller: passwordController,
-                            textInputAction: TextInputAction.next,
-                            decoration: const InputDecoration(
-                              hintText: "••••••••••",
-                              labelText: "Password",
-                            ),
-                          )
-                        ],
-                      ),
-                      actions: [
-                        TextButton(onPressed: pending ? null : () {
-                          setDialogState(() {
-                            pending = false;
-                            error = "";
-                          });
+                          actions: [
+                            TextButton(
+                                onPressed: pending
+                                    ? null
+                                    : () {
+                                        setDialogState(() {
+                                          pending = false;
+                                          error = "";
+                                        });
 
-                          Navigator.pop(context);
-                        }, child: Text("Cancel")),
-                        TextButton(onPressed: pending ? null : () async {
+                                        Navigator.pop(context);
+                                      },
+                                child: Text("Cancel")),
+                            TextButton(
+                                onPressed: pending
+                                    ? null
+                                    : () async {
+                                        setDialogState(() {
+                                          pending = true;
+                                          error = "";
+                                        });
 
-                          setDialogState(() {
-                            pending = true;
-                            error = "";
-                          });
+                                        try {
+                                          var userCredential =
+                                              EmailAuthProvider.credential(
+                                                  email: FirebaseAuth.instance
+                                                      .currentUser!.email!,
+                                                  password:
+                                                      passwordController.text);
+                                          await FirebaseAuth
+                                              .instance.currentUser!
+                                              .reauthenticateWithCredential(
+                                                  userCredential);
+                                          await FirebaseAuth
+                                              .instance.currentUser!
+                                              .delete();
 
-                          try {
-                            var userCredential = EmailAuthProvider.credential(email: FirebaseAuth.instance.currentUser!.email!, password: passwordController.text);
-                            await FirebaseAuth.instance.currentUser!.reauthenticateWithCredential(userCredential);
-                            await FirebaseAuth.instance.currentUser!.delete();
-
-                            Navigator.pushAndRemoveUntil(
-                              context,
-                              MaterialPageRoute(builder: (context) => Onboarding()), // Replace with your login screen
-                                  (Route<dynamic> route) => false, // Remove all previous routes
-                            );
-
-                          } on FirebaseAuthException catch (e) {
-                            if (e.code == 'invalid-credential') {
-                              setDialogState(() {
-                                error = "Invalid E-mail or password";
-                              });
-                              passwordController.clear();
-                            }
-                          } finally {
-                            setDialogState(() {
-                              pending = false;
-                            });
-                          }
-                        }, child: Text("Delete"))
-                      ],
-                    );
-                  });
-                });
+                                          if (context.mounted) {
+                                            Navigator.pushAndRemoveUntil(
+                                              context,
+                                              MaterialPageRoute(
+                                                  builder: (context) =>
+                                                      Onboarding()),
+                                              // Replace with your login screen
+                                              (Route<dynamic> route) =>
+                                                  false, // Remove all previous routes
+                                            );
+                                          }
+                                        } on FirebaseAuthException catch (e) {
+                                          if (e.code == 'invalid-credential') {
+                                            setDialogState(() {
+                                              error =
+                                                  "Invalid E-mail or password";
+                                            });
+                                            passwordController.clear();
+                                          }
+                                        } finally {
+                                          setDialogState(() {
+                                            pending = false;
+                                          });
+                                        }
+                                      },
+                                child: Text("Delete"))
+                          ],
+                        );
+                      });
+                    });
               },
             ),
             Divider(color: darkTheme.surfaceContainer, height: 1),
