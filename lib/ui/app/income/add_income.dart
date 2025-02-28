180c19d56a84e1a119a8d4ca749f09837f8ab1c4
Ahmed Helmy <125504430+AhmedHelmy18@users.noreply.github.com>
2025-02-28 14:49:22 +0200

    Switch to new pages when adding something instead of an alert dialog (#32)
    * make page addIncome to navigate to her from page income.
    
    * make the AddExpenses page to navigate from expenses page.
    
    * update the ui of addIncome and addExpenses.
    
    * make the addAssets and addLiabilities pages to navigate from icon add.
    
    * make add page to all pages.
    
    * refactor: remove .cxx and add it to .gitignore
    
    ---------
    
    Co-authored-by: Mohamed Eldahtoury <salintofficial@hotmail.com>

---
+++ lib/ui/app/income/add_income.dart
@@ -0,0 +1,139 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class AddIncome extends StatefulWidget {
+  const AddIncome({super.key});
+
+  @override
+  State<AddIncome> createState() => _AddIncomeState();
+}
+
+class _AddIncomeState extends State<AddIncome> {
+  bool pending = false;
+  String error = "";
+  TextEditingController nameController = TextEditingController();
+  TextEditingController valueController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Add a new income source",
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(right: 35.0, left: 35.0, top: 30.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            if (error.isNotEmpty)
+              Text(
+                error,
+                style: const TextStyle(
+                  color: Colors.red,
+                  fontSize: 15,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            TextField(
+              enabled: !pending,
+              controller: nameController,
+              textInputAction: TextInputAction.next,
+              decoration: const InputDecoration(
+                hintText: "Job",
+                labelText: "Name",
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TextField(
+              enabled: !pending,
+              controller: valueController,
+              textInputAction: TextInputAction.done,
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                hintText: "20000",
+                labelText: "Value",
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                TextButton(
+                  onPressed: pending
+                      ? null
+                      : () {
+                          Navigator.of(context).pop();
+
+                          nameController.clear();
+                          valueController.clear();
+                        },
+                  child: const Text(
+                    "Cancel",
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                  ),
+                ),
+                TextButton(
+                  onPressed: pending
+                      ? null
+                      : () async {
+                          setState(() {
+                            error = "";
+                            pending = true;
+                          });
+                          try {
+                            if (nameController.text.isEmpty ||
+                                valueController.text.isEmpty) {
+                              setState(() {
+                                error = "Please fill all fields";
+                              });
+                            } else {
+                              await FirebaseFirestore.instance
+                                  .collection("users")
+                                  .doc(FirebaseAuth.instance.currentUser?.uid)
+                                  .update(
+                                {
+                                  "income": FieldValue.arrayUnion([
+                                    {
+                                      "name": nameController.text,
+                                      "value": int.parse(valueController.text)
+                                    }
+                                  ])
+                                },
+                              );
+                              nameController.clear();
+                              valueController.clear();
+
+                              if (context.mounted) {
+                                Navigator.of(context).pop();
+                              }
+                            }
+                          } on Exception {
+                            setState(() {
+                              error = "An error has occurred";
+                            });
+                          } finally {
+                            setState(() {
+                              pending = false;
+                            });
+                          }
+                        },
+                  child: const Text(
+                    "Add",
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
