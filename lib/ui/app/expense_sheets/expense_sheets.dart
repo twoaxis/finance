38b1f72171723542348e499086023b4cbd971d70
Mohamed Eldahtoury <salintofficial@hotmail.com>
2025-03-04 16:22:34 +0200

    feat: add ability to delete expense sheets

--- lib/ui/app/expense_sheets/expense_sheets.dart
+++ lib/ui/app/expense_sheets/expense_sheets.dart
@@ -1,6 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:financial_planner_mobile/cubit/expenses_cubit.dart';
 import 'package:financial_planner_mobile/ui/app/expense_sheets/expense_sheet_items.dart';
 import 'package:financial_planner_mobile/util/theme.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -24,6 +26,38 @@ class _ExpenseSheetsPageState extends State<ExpenseSheetsPage> {
                 itemBuilder: (BuildContext context, int index) {
                   return ListTile(
                     title: Text(expenses[index].data()["name"]),
+                    onLongPress: () async {
+                      showDialog(
+                          context: context,
+                          builder: (BuildContext build) {
+                            return AlertDialog(
+                              title: Text("Are you sure?"),
+                              content: Text(
+                                  "Deleting \"${expenses[index].data()["name"]}\" will delete all expenses within it permanently.",),
+                              icon: const Icon(Icons.delete),
+                              actions: [
+                                TextButton(
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                  child: const Text("Cancel"),
+                                ),
+                                TextButton(
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+
+                                    // TODO: Error handling.
+                                    FirebaseFirestore.instance
+                                        .collection("users")
+                                        .doc(FirebaseAuth.instance
+                                        .currentUser?.uid).collection("expenses").doc(expenses[index].id).delete();
+                                  },
+                                  child: const Text("Yes"),
+                                ),
+                              ],
+                            );
+                          });
+                    },
                     onTap: () {
                       Navigator.push(
                         context,
