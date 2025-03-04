7f19b73dd27cbd1c58f2eef280e915c05892f0be
Mohamed Eldahtoury <salintofficial@hotmail.com>
2025-03-04 15:08:25 +0200

    feat: update add balance screen to new UI

--- lib/ui/app/balances/balances_add.dart
+++ lib/ui/app/balances/balances_add.dart
@@ -1,4 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:financial_planner_mobile/ui/common/primary_button.dart';
+import 'package:financial_planner_mobile/util/theme.dart';
+import 'package:financial_planner_mobile/values/spaces.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -24,110 +27,99 @@ class _BalancesAddState extends State<BalancesAdd> {
         ),
       ),
       body: Padding(
-        padding: const EdgeInsets.only(right: 35.0, left: 35.0, top: 30.0),
+        padding: EdgeInsets.all(fullscreenSpacing),
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
-            if (error.isNotEmpty)
-              Text(
-                error,
-                style: const TextStyle(
-                  color: Colors.red,
-                  fontSize: 15,
-                  fontWeight: FontWeight.w500,
+            Expanded(
+              child: SizedBox(
+                width: double.infinity,
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      TextField(
+                        enabled: !pending,
+                        controller: nameController,
+                        textInputAction: TextInputAction.next,
+                        decoration: InputDecoration(
+                          hintText: "Job",
+                          labelText: "Name",
+                          border: OutlineInputBorder(
+                            borderSide:
+                            BorderSide(color: darkTheme.surfaceBright),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      TextField(
+                        enabled: !pending,
+                        controller: valueController,
+                        textInputAction: TextInputAction.done,
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(
+                          hintText: "200",
+                          labelText: "Value",
+                          border: OutlineInputBorder(
+                            borderSide:
+                            BorderSide(color: darkTheme.surfaceBright),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      SizedBox(
+                        height: 20,
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            TextField(
+            ),
+            PrimaryButton(
+              text: "Add balance",
               enabled: !pending,
-              controller: nameController,
-              textInputAction: TextInputAction.next,
-              decoration: const InputDecoration(
-                hintText: "Bank account",
-                labelText: "Name",
-              ),
-            ),
-            TextField(
-              enabled: !pending,
-              controller: valueController,
-              textInputAction: TextInputAction.done,
-              keyboardType: TextInputType.number,
-              decoration: const InputDecoration(
-                hintText: "20000",
-                labelText: "Value",
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                TextButton(
-                  onPressed: pending
-                      ? null
-                      : () {
-                    Navigator.of(context).pop();
-
+              onPressed: () async {
+                setState(() {
+                  error = "";
+                  pending = true;
+                });
+                try {
+                  if (nameController.text.isEmpty ||
+                      valueController.text.isEmpty) {
+                    setState(() {
+                      error = "Please fill all fields";
+                    });
+                  } else {
+                    await FirebaseFirestore.instance
+                        .collection("users")
+                        .doc(FirebaseAuth.instance.currentUser?.uid)
+                        .update(
+                      {
+                        "balances": FieldValue.arrayUnion([
+                          {
+                            "name": nameController.text,
+                            "value": int.parse(valueController.text)
+                          }
+                        ])
+                      },
+                    );
                     nameController.clear();
                     valueController.clear();
-                  },
-                  child: const Text(
-                    "Cancel",
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
-                  ),
-                ),
-                TextButton(
-                  onPressed: pending
-                      ? null
-                      : () async {
-                    setState(() {
-                      error = "";
-                      pending = true;
-                    });
-                    try {
-                      if (nameController.text.isEmpty ||
-                          valueController.text.isEmpty) {
-                        setState(() {
-                          error = "Please fill all fields";
-                        });
-                      } else {
-                        await FirebaseFirestore.instance
-                            .collection("users")
-                            .doc(FirebaseAuth.instance.currentUser?.uid)
-                            .update(
-                          {
-                            "balances": FieldValue.arrayUnion(
-                              [
-                                {
-                                  "name": nameController.text,
-                                  "value": int.parse(valueController.text)
-                                }
-                              ],
-                            ),
-                          },
-                        );
-                        nameController.clear();
-                        valueController.clear();
 
-                        if (context.mounted) Navigator.of(context).pop();
-                      }
-                    } on Exception {
-                      setState(() {
-                        error = "An error has occurred";
-                      });
-                    } finally {
-                      setState(() {
-                        pending = false;
-                      });
+                    if (context.mounted) {
+                      Navigator.of(context).pop();
                     }
-                  },
-                  child: const Text(
-                    "Add",
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
-                  ),
-                ),
-              ],
-            ),
+                  }
+                } on Exception {
+                  setState(() {
+                    error = "An error has occurred";
+                  });
+                } finally {
+                  setState(() {
+                    pending = false;
+                  });
+                }
+              },
+            )
           ],
         ),
       ),
