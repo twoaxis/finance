782cdb3920c41f54890f4132ca49e6a8be9e02a3
Mohamed Eldahtoury <salintofficial@hotmail.com>
2025-03-14 18:01:12 +0200

    build: bump version

--- lib/ui/app/info/info.dart
+++ lib/ui/app/info/info.dart
@@ -18,7 +18,7 @@ class InfoPage extends StatelessWidget {
             ListTile(
               title: Text("Version",
                   style: TextStyle(color: darkTheme.onSurfaceVariant)),
-              subtitle: Text("4.0.1-beta",
+              subtitle: Text("4.1.0-beta",
                   style: TextStyle(color: darkTheme.onSurfaceVariant)),
               leading: Icon(Icons.build, color: darkTheme.onSurfaceVariant),
               onTap: () {},
@@ -27,7 +27,7 @@ class InfoPage extends StatelessWidget {
             ListTile(
               title: Text("Build Number",
                   style: TextStyle(color: darkTheme.onSurfaceVariant)),
-              subtitle: Text("9",
+              subtitle: Text("10",
                   style: TextStyle(color: darkTheme.onSurfaceVariant)),
               leading: Icon(Icons.build, color: darkTheme.onSurfaceVariant),
               onTap: () {},
