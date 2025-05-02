159f042d4ca9f7e70aef3ab3213ca14a861685d1
Mohamed Eldahtoury <salintofficial@hotmail.com>
2025-05-02 09:45:32 +0300

    refactor: put mobile app in mobile/ folder

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -27,8 +27,8 @@ void main() async {
   );
 
   if(kDebugMode) {
-    //await FirebaseAuth.instance.useAuthEmulator("10.0.2.2", 9099);
-    //FirebaseFirestore.instance.useFirestoreEmulator("10.0.2.2", 8080);
+    await FirebaseAuth.instance.useAuthEmulator("10.0.2.2", 9099);
+    FirebaseFirestore.instance.useFirestoreEmulator("10.0.2.2", 8080);
   }
 
   runApp(
